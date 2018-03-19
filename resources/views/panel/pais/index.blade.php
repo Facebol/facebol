6df31e3a272c4dc8b30aba6e9a9de1f366ca8653
marco <mark_mihal@hotmail.com>
2018-03-19 12:22:57 -0400

    nuevoos

---
+++ resources/views/panel/pais/index.blade.php
@@ -0,0 +1,45 @@
+@extends('panel.template')
+@section('contenido')
+<section class="content">
+        <div class="col-xs-12">                            
+            <div class="box">
+                <div class="box-header">
+                    <h2 class="text-center">Lista de Empresas</h2>
+                </div><!-- /.box-header -->
+                <div class="col-xs-12 col-md-6" style="padding-left: 920px;">
+                    <a href="{{route('pais.create')}}">
+                        <button type="button" class="btn btn-success"><i class="fa fa-plus"></i>  &nbsp;&nbsp;&nbsp;&nbsp; Agregar</button>
+                    </a>
+                </div><br><br><br>
+                <div class="box-body table-responsive">
+                    <table class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Opciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($pais as $pais)
+                            <tr>
+                                <td>{{$pais->nombre}}</td>
+                                <td>
+                                  {!!Form::open(['route'=>['pais.destroy', $pais->id], 'method' => 'DELETE'])!!}
+                                  <a href="{{route('pais.edit',$pais->id)}}" class="btn btn-primary"><i class="fa fa-pencil"></i></a>
+                                  <button class="btn btn-success"><i class="fa fa-check"></i></button>
+                                  {!!Form::close()!!}
+                                </td> 
+                            </tr>                        
+                            @endforeach                   
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <th>Nombre</th>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div><!-- /.box-body -->
+            </div><!-- /.box -->
+        </div>
+</section><!-- /.content -->
+@stop
--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ Route::post('log','controllerLogin@log')->name('log');
 Route::get('logout','controllerLogin@logout')->name('logout');
 Route::post('reset','controllerInicio@emailReset')->name('reset');
 //panel
+Route::resource('panel/ciudad','controladorCiudad');
+Route::resource('panel/pais','controladorPais');
 Route::resource('panel/empresas','controllerEmpresa');
 Route::resource('panel/usuarios','controllerUsuario');
 Route::resource('panel/institucion','controllerInstitucion');
